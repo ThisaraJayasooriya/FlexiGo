4b1df9d262183f68f35ff2e6d6a6088353a31417
Thisara-Jayasooriya <jthisaradj2001@gmail.com>
2025-12-25 19:09:36 +0530

    fixed dashboard zoom in issue on i phone screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="icon" href="/icons/flexigo_logo.jpg" />
         <link rel="apple-touch-icon" href="/icons/flexigo_logo.jpg" />
         <meta name="theme-color" content="#124E66" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
       <body className="bg-[#F8F9FA] text-[#124E66]">
         <ServiceWorkerRegistration />
